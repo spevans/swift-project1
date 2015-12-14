1b4d0491d4351cae170e7a49500fc516c8a26674
Simon Evans <si@si.org>
2015-12-14 00:19:33 +0000

    Fixes for fakelib/linux_libcpp.c
    - std::string::_S_construct():
        return correct ptr type
        memcpy() the correct number of bytes
        always ensure string is NUL terminated
        fixup callers to use the return value properly
    
    - std::basic_string(std::string const&):
        update the refcount on the correct object
    
    - These fixes fixup the error that was exposed by
      dump_basic_string()

--- fakelib/linux_libcpp.c
+++ fakelib/linux_libcpp.c
@@ -20,6 +20,7 @@
 //_operator delete(void*)
 void _ZdlPv(void *this) {
         //kprintf("%p->delete()\n", this);
+        free(this);
 }
 
 
@@ -73,14 +74,6 @@ struct basic_string {
 };
 
 
-static inline struct basic_string *
-this_from_string(char *str)
-{
-        struct basic_string *this = (struct basic_string *)str;
-        return this - 1;
-}
-
-
 // std::string::_Rep::_S_empty_rep_storage
 // Empty string with all fields set to 0
 struct basic_string _ZNSs4_Rep20_S_empty_rep_storageE[2] = {};
@@ -109,16 +102,7 @@ string_set_shareable(struct basic_string *str)
 static inline void
 dump_basic_string(struct basic_string *this)
 {
-        // FIXME: This overflow memory due to large strings exceeding kprintf's internal buffer. That needs to be fixed.
-        // Also indicates some strings are not being NUL terminated. Uncommenting this causes an OOPS at startup
-        //kprintf("DBS %p->(%lu,%lu,%d,\"%s\")\n", this, this->length, this->capacity, this->refcount, this->data);
-}
-
-
-static inline void
-dump_string(char *str)
-{
-        dump_basic_string(this_from_string(str));
+        kprintf("DBS %p->(%lu,%lu,%d,\"%s\")\n", this, this->length, this->capacity, this->refcount, this->data);
 }
 
 
@@ -136,7 +120,7 @@ _ZNSs4_Rep9_S_createEmmRKSaIcE(size_t capacity, size_t old_capacity, void *alloc
                 capacity = 2 * old_capacity;
         }
 
-        if (capacity  > str_max_size) {
+        if (capacity > str_max_size) {
                 capacity = str_max_size;
         }
 
@@ -161,22 +145,23 @@ _ZNSs4_Rep8_M_cloneERKSaIcEm(struct basic_string *this, void *allocator, size_t 
         size_t capacity = len + this->length;
         result = _ZNSs4_Rep9_S_createEmmRKSaIcE(capacity, this->capacity, allocator);
 
-        if (this->length) {
-                if (this->length != 1) {
-                        memcpy(result->data, this->data, this->length);
-                } else {
-                        if (result != _ZNSs4_Rep20_S_empty_rep_storageE) {
-                                result->data[0] = this->data[0];
-                        }
-                }
-                result->data[this->length] = '\0';
-        }
-        if (result != _ZNSs4_Rep20_S_empty_rep_storageE) {
-                result->length = this->length;
-                result->refcount = 0;
+        if (unlikely(this->length == 0)) {
+                return result + 1;
         }
 
-        return result+1;
+        if (likely(this->length != 1)) {
+                memcpy(result->data, this->data, this->length);
+        } else {
+                result->data[0] = this->data[0];
+        }
+
+        if (likely(result != _ZNSs4_Rep20_S_empty_rep_storageE)) {
+                result->length = this->length;
+                result->refcount = 0;
+                result->data[this->length] = '\0';
+        }
+
+        return result + 1;
 }
 
 
@@ -185,7 +170,6 @@ _ZNSs4_Rep8_M_cloneERKSaIcEm(struct basic_string *this, void *allocator, size_t 
 void
 _ZNSs4_Rep10_M_destroyERKSaIcE(struct basic_string *this, void *allocator)
 {
-        //kprintf("%p->_M_destroy(%p)\n", this, allocator);
         dump_basic_string(this);
         free(this);
 }
@@ -201,7 +185,7 @@ _ZNSs9_M_mutateEmmm(struct basic_string *this, size_t pos, size_t len1, size_t l
 
 
 // char* std::string::_S_construct<char const*>(char const*, char const*, std::allocator<char> const&, std::forward_iterator_tag)
-char *
+struct basic_string *
 _ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag(const char *str_start, const char *str_end, void *allocator, int tag)
 {
         //kprintf("_S_construct(%s, %s, %p, %p, %p, %d) sizeof=%lu\n", str_start, str_end, str_start, str_end, allocator, tag, sizeof(struct basic_string));
@@ -210,7 +194,7 @@ _ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag(const char *str_
                 // return statically allocated empty string
                 kprintf("returning empty storage %p", _ZNSs4_Rep20_S_empty_rep_storageE + 1);
                 dump_basic_string(_ZNSs4_Rep20_S_empty_rep_storageE);
-                return (char *)(_ZNSs4_Rep20_S_empty_rep_storageE + 1);
+                return _ZNSs4_Rep20_S_empty_rep_storageE + 1;
         }
         if (str_start == NULL && str_end != NULL) {
                 koops("_S_construct with null start");
@@ -218,15 +202,21 @@ _ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag(const char *str_
 
         size_t size = str_end - str_start;
 
-        struct basic_string *res = _ZNSs4_Rep9_S_createEmmRKSaIcE(size, 0, allocator);
+        struct basic_string *result = _ZNSs4_Rep9_S_createEmmRKSaIcE(size, 0, allocator);
+        if (likely(size != 1)) {
+                memcpy(result->data, str_start, size);
+        } else {
+                result->data[0] = str_start[0];
+        }
 
-        char *string = memcpy(res->data, str_start, size+1);
+        if (likely(result != _ZNSs4_Rep20_S_empty_rep_storageE)) {
+                 result->length = size;
+                 result->refcount = 0;
+                 result->data[size] = '\0';
+        }
+        dump_basic_string(result);
 
-        res->length = size;
-        res->refcount = 0;
-        dump_basic_string(res);
-
-        return string;   // return pointer to end
+        return result+1;   // return pointer to end
 }
 
 
@@ -234,13 +224,10 @@ _ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag(const char *str_
 void
 _ZNSsC1EPKcmRKSaIcE(struct basic_string **this_p, char const *string, unsigned int len, void **allocator)
 {
-        kprintf("_ZNSsC1EPKcmRKSaIcE(%p,%s,%u,%p)\n", this_p, string, len, allocator);
-        //struct basic_string *this = (*this_p)-1;
-
-        //kprintf("%p->basic_string(%s, %u)", this, string, len);
-        char *res = _ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag(string, string + len, allocator, 0);
-        dump_string(res);
-        *this_p = (struct basic_string *)&res;
+        //kprintf("_ZNSsC1EPKcmRKSaIcE(%p,%s,%u,%p)\n", this_p, string, len, allocator);
+        struct basic_string *result = _ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag(string, string + len, allocator, 0);
+        dump_basic_string(result - 1);
+        *this_p = result;
 }
 
 
@@ -250,25 +237,19 @@ _ZNSsC1ERKSs(struct basic_string **this_p, struct basic_string **that_p)
 {
         struct basic_string *this = (*this_p) - 1;
         struct basic_string *that = (*that_p) - 1;
-
-        kprintf("_ZNSsC1ERKS(%p[%p],%p[%p]): ", this_p, this, that_p, that);
+        //kprintf("_ZNSsC1ERKSs(%p[%p],%p[%p]): ", this_p, this, that_p, that);
 
         if (that->refcount >= 0) {
                 if (that == _ZNSs4_Rep20_S_empty_rep_storageE) {
                         *this_p = *that_p;
                 } else {
-                        // Always false
-                        /***if (__pthread_key_create == NULL) {
-                                // With no pthreads, the refcount doenst need an atomic inc
-                                that->refcount++;
-                                } else {***/
-                                __atomic_fetch_add(&this->refcount, 1, __ATOMIC_RELAXED);
-                                *this_p = *that_p;
+                        __atomic_fetch_add(&that->refcount, 1, __ATOMIC_RELAXED);
+                        *this_p = *that_p;
                 }
         } else {
                 *this_p = _ZNSs4_Rep8_M_cloneERKSaIcEm(that, NULL, 0);
         }
-        kprintf("= %p\n", *this_p);
+        //kprintf("= %p\n", *this_p);
 }
 
 
@@ -281,9 +262,9 @@ _ZNSsC1EPKcRKSaIcE(struct basic_string **this_p, char const *string, void *alloc
         char const *end =  string;
         end += (string != NULL) ? strlen(string) : npos;
 
-        char *res = _ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag(string, end, allocator, 0);
-        dump_string(res);
-        *this_p = (struct basic_string *)res;
+        struct basic_string *result = _ZNSs12_S_constructIPKcEEPcT_S3_RKSaIcESt20forward_iterator_tag(string, end, allocator, 0);
+        dump_basic_string(result - 1);
+        *this_p = result;
         print_string("_ZNSsC1EPKcRKSaIcE finished\n");
         hlt();
 }
@@ -298,6 +279,7 @@ _ZNKSs4findEPKcmm(struct basic_string **this_p, const char *string, size_t pos, 
         size_t size = this->length;
 
         kprintf("find(%s, %s, %lu,%lu)\n", (char *)this_p, string, pos, len);
+        hlt();
         dump_basic_string(this);
         if (len == 0) {
                 return pos < size ? pos : npos;
@@ -329,6 +311,7 @@ _ZNKSs7compareEPKc(struct basic_string **this_p, const char *string)
         size_t len = size < osize ? size : osize;
 
         kprintf("compare(%s, %s, %lu)\n", (char *)this_p, string, size);
+        hlt();
         dump_basic_string(this);
 
         int result = memcmp(string_data, string, len);
