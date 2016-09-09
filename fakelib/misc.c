632312b9d73622405c17f57a376325fdad713f6a
Simon Evans <si@si.org>
2016-09-09 12:33:21 +0100

    fakelib: Remove emulation of libdl
    - fakelib/misc.c: Remove libdl functions dlopen, dlsym,
      dladdr, dlclose and dl_iterate_phdr
    
    - kernel-lib now contains a fix in ProtocolConformance.cpp
      to use `__swift2_type_metadata_start' if set by the linker
      and bypasses the need for libdl
    
    - linker.script: Rename `swift2_type_metadata_start' to
      `__swift2_type_metadata_start'

--- fakelib/misc.c
+++ fakelib/misc.c
@@ -9,20 +9,12 @@
  */
 
 #include "klibc.h"
-#define __USE_GNU
-#include <link.h>
-#include <dlfcn.h>
 
 #define _UNISTD_H   // Avoid including all of unistd.h
 #include <bits/confname.h>
 
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-
-
-extern void *swift2_protocol_conformances_start;
-// Dummy empty structure for dl_iterate_phdr
-static struct dl_phdr_info empty_dl_phdr_info = { .dlpi_addr = 0 };
 
 
 void
@@ -69,79 +61,6 @@ munmap(void *addr, size_t length)
 
         return 0;
 }
-
-
-// Fake a handle if opening NULL (this binary) else oops
-void *
-dlopen(const char *filename, int flag)
-{
-        if (filename != NULL) {
-                koops("dlopen called with filename=%s", filename);
-        }
-
-        debugf("dlopen(%s,%X)\n", filename, flag);
-        return (void *)1;
-}
-
-
-// Hardcoded to allow lookup of 1 known symbol, enough for now
-void *
-dlsym(void *handle, const char *symbol)
-{
-        debugf("dlsym(%p, \"%s\")=", handle, symbol);
-        if (handle != (void *)1) {
-                koops("dlsym(): bad handle: %p", handle);
-        }
-        if (!strcmp(symbol, ".swift2_protocol_conformances_start")) {
-                debugf("%p\n", &swift2_protocol_conformances_start);
-
-                return &swift2_protocol_conformances_start;
-        } else {
-                koops("dlsym(): bad symbol: %s\n", symbol);
-        }
-}
-
-
-int
-dladdr(const void *addr, Dl_info *info)
-{
-        // FIXME: Dummy implementation for now, returns `not found'
-        kprintf("dladdr() required for %p\n", addr);
-
-        // info declared as nonnull in dlfcn.h
-        info->dli_fname = NULL;
-        info->dli_fbase = NULL;
-        info->dli_sname = NULL;
-        info->dli_saddr = NULL;
-
-        return 0;
-}
-
-
-// Sanity check the handle
-int
-dlclose(void *handle)
-{
-        debugf("dlclose(%p)\n", handle);
-        if (handle != (void *)1) {
-                koops("dlclose(): bad handle: %p", handle);
-        }
-        return 0;
-}
-
-
-// Hardcoded to return an empty structure, the caller of this only
-// cares about the filename anyway and can deal with it being NULL
-int
-dl_iterate_phdr(int (*callback) (struct dl_phdr_info *info,
-                                 size_t size, void *data), void *data)
-{
-        debugf("dl_iterate_phdr(%p,%p)\n", callback, data);
-        int res = callback(&empty_dl_phdr_info, sizeof(struct dl_phdr_info), data);
-
-        return res;
-}
-
 
 
 long
