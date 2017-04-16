f2e114b812b0786dabd8f68c23cb67089819db05
Simon Evans <si@si.org>
2017-04-16 12:20:47 +0100

    mm: Use more > 64 allocations for 32/48 byte malloc()
    - Use 32byte alloctions for malloc and set the number of entries
      per page > 64 for 32 and 48 byte allocations. Use both allocation
      bitmaps for these 2 sizes.
    
    - Reverse the meaning of the bits in the allocation bitmap so
      that bitmap_mask() is only called when a new page is allocated.
    
    - Make the pages allocated for each size into a doubly linked list
      by adding a *prev, although pages are not yet freed when they
      are fully empty.
    
    - These changes allow reducing the size of the initial heap set in
      linker.script from 64 to 60 pages when booting on a QEMU 16G
      machine.

--- klibc/malloc.c
+++ klibc/malloc.c
@@ -26,6 +26,7 @@
  */
 
 #include <stdatomic.h>
+#include <assert.h>
 #include "klibc.h"
 #include "mm.h"
 
@@ -41,12 +42,14 @@ struct slab_block_info {
 // Should be 64 bytes
 struct slab_header {
         uint64_t slab_size;
+        // Bits in bitmap are 0=allocated 1=free
         uint64_t allocation_bm[2];
         struct slab_header *next;
+        struct slab_header *prev;
 
         /* debug */
-        uint64_t malloc_cnt;
-        uint64_t free_cnt;
+        uint32_t malloc_cnt;
+        uint32_t free_cnt;
 #ifdef MALLOC_DEBUG
         char signature[8];
         uint64_t checksum;
@@ -55,6 +58,7 @@ struct slab_header {
 #endif
         char data[4032];                // upto a page
 } __attribute__((aligned(4096),packed));
+
 
 
 struct malloc_region {
@@ -70,10 +74,11 @@ struct malloc_region {
 // waste a few bytes at the end of the block. All blocks returned are
 // aligned to 16bytes.
 static const struct slab_block_info slab_info[] = {
-        { 48, 64 },
-        { 64, 63 },
-        { 80, 50 },
-        { 96, 42 },
+        { 32, 126 },
+        { 48, 84  },
+        { 64, 63  },
+        { 80, 50  },
+        { 96, 42  },
         { 112, 36 },
         { 128, 31 },
         { 144, 28 },
@@ -126,21 +131,133 @@ update_checksum(struct slab_header *slab)
 }
 
 
+// Mask of bits used in the allocation bitmap
+static inline uint64_t
+bitmap_mask(int count)
+{
+        assert(count >= 0 && count <= 64);
+        if (count == 64) {
+                return UINT64_MAX;
+        }
+        uint64_t mask = 1;
+        uint64_t result = (mask << count) - 1;
+
+        return result;
+}
+
+
+static inline void
+set_bitmap_mask(struct slab_header *slab, int slab_idx)
+{
+        assert(slab_idx >= 0);
+        assert(slab_idx < (int)SLAB_SIZES);
+        int count = slab_info[slab_idx].slab_count;
+        int lo_count = (count < 64) ? count : 64;
+        int hi_count = (count > 64) ? count - 64 : 0;
+        slab->allocation_bm[0] = bitmap_mask(lo_count);
+        slab->allocation_bm[1] = bitmap_mask(hi_count);
+}
+
+
+static inline void
+set_bitmap_entry(struct slab_header *slab, int bit_idx)
+{
+        assert(bit_idx < 128);
+        int idx = bit_idx / 64;
+        int bit = bit_idx % 64;
+        uint64_t mask = 1 << bit;
+        debugf("set_bitmap_entry: bit_idx=%d idx=%d bit=%d bm: %16.16lx %16.16lx mask: %"PRIx64 "\n",
+               bit_idx, idx, bit, slab->allocation_bm[1],
+               slab->allocation_bm[0], mask);
+        slab->allocation_bm[idx] |= mask;
+        debugf("set_bitmap_entry: bm[0]=%"PRIx64 " bm[1]=%"PRIx64 "\n",
+               slab->allocation_bm[0], slab->allocation_bm[1]);
+}
+
+
+static inline void
+clear_bitmap_entry(struct slab_header *slab, int bit_idx)
+{
+        assert(bit_idx < 128);
+        int idx = bit_idx / 64;
+        int bit = bit_idx % 64;
+        uint64_t mask = 1 << bit;
+        debugf("clear_bitmap_entry: bit_idx=%d idx=%d bit=%d bm: %16.16lx %16.16lx mask: %"PRIx64 "\n",
+               bit_idx, idx, bit, slab->allocation_bm[1],
+               slab->allocation_bm[0], mask);
+        slab->allocation_bm[idx] &= ~mask;
+
+        debugf("clear_bitmap_entry: bm[0]=%"PRIx64 " bm[1]=%"PRIx64 "\n",
+               slab->allocation_bm[0], slab->allocation_bm[1]);
+}
+
+
+static inline int
+test_bitmap_entry(struct slab_header *slab, int bit_idx)
+{
+        assert(bit_idx < 128);
+        int idx = bit_idx / 64;
+        int bit = bit_idx % 64;
+        uint64_t mask = 1 << bit;
+        return (slab->allocation_bm[idx] & mask) != 0;
+}
+
+
+// Returns bit_idx (0-127) of lowest bit that is set, -1 if none found
+static int
+find_lowest_bit(struct slab_header *slab, int slab_idx)
+{
+        assert(slab_idx >= 0);
+        assert(slab_idx < (int)SLAB_SIZES);
+        int count = slab_info[slab_idx].slab_count;
+        assert(count > 0);
+        assert(count < 128);
+        int lo_count = (count < 64) ? count : 64;
+        int hi_count = (count > 64) ? count - 64 : 0;
+
+        debugf("find_lowest_bit: slab=%p bm=%16.16lx %16.16lx count=%d lo=%d hi=%d\n",
+               slab, slab->allocation_bm[1], slab->allocation_bm[0],
+               count, lo_count, hi_count);
+
+        int freebit = __builtin_ffsl(slab->allocation_bm[0]);
+        if (freebit == 0 && hi_count > 0) {
+                freebit = __builtin_ffsl(slab->allocation_bm[1]);
+                if (freebit == 0) {
+                        return -1;
+                }
+                assert(freebit < hi_count+1);
+                freebit += 63;
+        } else {
+                freebit--;
+                assert(freebit < lo_count);
+        }
+        assert(freebit >= -1 && freebit < 128);
+        debugf("find_lowest_bit: slab=%p bm=%16.16lx %16.16lx freebit=%d\n",
+               slab, slab->allocation_bm[1], slab->allocation_bm[0], freebit);
+        return freebit;
+}
+
+
+
 // Convert a page into a slab
 static struct slab_header *
 add_new_slab(int slab_idx)
 {
         struct slab_header *slab = alloc_pages(1);
         slab->slab_size = slab_info[slab_idx].slab_size;
-        slab->allocation_bm[0] = 0;
-        slab->allocation_bm[1] = 0;
-        slab->next = slabs[slab_idx];
+        set_bitmap_mask(slab, slab_idx);
+        slab->prev = NULL;
+        struct slab_header *current_head = slabs[slab_idx];
+        slab->next = current_head;
+        if (current_head) {
+                current_head->prev = slab;
+        }
+        slabs[slab_idx] = slab;
+
 #ifdef MALLOC_DEBUG
         strcpy(slab->signature, "MALLOC");      // for debugging
         update_checksum(slab);
 #endif
-        slabs[slab_idx] = slab;
-
         return slab;
 }
 
@@ -186,26 +303,10 @@ map_size_to_idx(size_t size)
 }
 
 
-// Mask of bits used in the allocation bitmap
-static inline uint64_t
-bitmap_mask(int slab_idx)
-{
-        uint64_t count = (uint64_t)slab_info[slab_idx].slab_count;
-        if (count == 64) {
-                return UINT64_MAX;
-        }
-        uint64_t mask = 1;
-        uint64_t result = (mask << count) - 1;
-
-        return result;
-}
-
-
 void *
 malloc(size_t size)
 {
         void *retval = NULL;
-        //debugf("malloc(%lu): ", size);
         if (sizeof(struct slab_header) != PAGE_SIZE) {
                 koops("slab_header is %lu bytes", sizeof(struct slab_header));
         }
@@ -225,7 +326,7 @@ malloc(size_t size)
                 size_t pages = (sizeof(struct malloc_region) + size + PAGE_MASK) / PAGE_SIZE;
                 struct malloc_region *result = alloc_pages(pages);
                 result->region_size = (pages * PAGE_SIZE) - sizeof(struct malloc_region);
-                debugf("Wanted %lu got %u\n", size, result->region_size);
+                debugf("Wanted %lu got %lu\n", size, result->region_size);
                 retval = result->data;
         } else {
                 int slab_idx = map_size_to_idx(size);
@@ -236,25 +337,23 @@ malloc(size_t size)
                         freebit = 0;
                 } else {
                         validate_is_slab(slab);
-                        uint64_t allocation_mask = bitmap_mask(slab_idx);
-                        uint64_t free_bits = slab->allocation_bm[0] ^ allocation_mask;
-                        freebit = __builtin_ffsl(free_bits);
-
-                        if (unlikely(freebit == 0)) {
+                        freebit = find_lowest_bit(slab, slab_idx);
+                        if (unlikely(freebit == -1)) {
                                 slab = add_new_slab(slab_idx);
-                        } else {
-                                freebit--;
+                                freebit = 0;
                         }
                 }
+                assert(freebit >= 0 && freebit < 128);
                 size_t offset = freebit * slab_info[slab_idx].slab_size;
+                debugf("malloc(%lu) slab=%p offset=%lx freebit=%d bm=%16.16lx %16.16lx\n",
+                       size, slab, offset, freebit, slab->allocation_bm[1], slab->allocation_bm[0]);
                 retval = &slab->data[offset];
-
-                uint64_t free_mask = (uint64_t)1 << freebit;
-                slab->allocation_bm[0] |= free_mask;
+                clear_bitmap_entry(slab, freebit);
                 slab->malloc_cnt++;
                 update_checksum(slab);
-                debugf("malloc(%lu)=%p slab=%p offset=%lx [%"PRIu64 "/%"PRIu64"]\n",
-                       size, retval, slab, offset, slab->malloc_cnt, slab->free_cnt);
+                debugf("malloc(%lu)=%p slab=%p offset=%lx [%u/%u] freebit=%d bm=%16.16lx %16.16lx\n",
+                       size, retval, slab, offset, slab->malloc_cnt, slab->free_cnt, freebit,
+                       slab->allocation_bm[1], slab->allocation_bm[0]);
         }
         if (atomic_fetch_sub(&malloc_lock, 1) != 1) {
                 koops("(malloc)malloc_lock != 1");
@@ -287,11 +386,8 @@ free(void *ptr)
                 free_pages(slab, pages);
         } else {
                 validate_is_slab(slab);
-                debugf("slab=%p ", slab);
-                debugf("cs=%"PRIx64 "\n", slab->checksum);
-                debugf("size=%u  ", slab->slab_size);
                 size_t offset = (ptr - (void *)slab);
-                debugf("offset=%"PRIu64, offset);
+                debugf("slab=%p size=%lu  offset=%"PRIu64 "\n", slab, slab->slab_size, offset);
                 if (unlikely(offset < 64)) {
                         koops("free(%p) offset = %lu", ptr, offset);
                 }
@@ -300,22 +396,22 @@ free(void *ptr)
                               ptr, slab->slab_size, offset - 64);
                 }
                 int bit_idx = (offset-64) / slab->slab_size;
-                uint64_t bitmap_mask = (uint64_t)1 << bit_idx;
-                debugf("  bit_idx = %d mask=%"PRIx64"\n", bit_idx, bitmap_mask);
-                if (likely(slab->allocation_bm[0] & bitmap_mask)) {
-                        slab->allocation_bm[0] &= ~bitmap_mask;
+                debugf("  bit_idx = %d bm=%16.16lx %16.16lx\n", bit_idx,
+                       slab->allocation_bm[1], slab->allocation_bm[0]);
+                if (likely(test_bitmap_entry(slab, bit_idx) == 0)) {
+                        set_bitmap_entry(slab, bit_idx);
                         slab->free_cnt++;
-                        debugf(" alloc_bm = %"PRIx64 " freecnt=%"PRIu64 " ",
-                               slab->allocation_bm[0], slab->free_cnt);
                 } else {
-                        koops("%p is not allocated, alloc=%"PRIx64 " mask = %"PRIx64,
-                              ptr, slab->allocation_bm[0], bitmap_mask);
+                        koops("%p is not allocated, alloc=%"PRIx64 "\n",
+                              ptr, slab->allocation_bm[0]);
                 }
 #ifdef MALLOC_DEBUG
                 memset(ptr, 0xAA, slab->slab_size);
-#endif
                 update_checksum(slab);
                 debugf("\ncs=%"PRIx64 "\n", slab->checksum);
+#else
+                debugf("\n");
+#endif
         }
         if (atomic_fetch_sub(&malloc_lock, 1) != 1) {
                 koops("(free)malloc_lock != 1");
